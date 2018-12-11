c10da296689b64cca288a4e0247b9f22096c2314
Long Wang <long.w@columbia.edu>
2018-12-11 13:29:35 -0500

    modified scrips to updated repo

--- update_repo.sh
+++ update_repo.sh
@@ -2,11 +2,16 @@
 # update all submodules
 # Assuming you are at the arm_teleop_ur5 directory level
 git pull
+git submodule update --init --recursive
 src_path=$(pwd)
 ur_sub_dir="/universal_robot"
+teleop_keyboard_dir="/user_input/teleop_twist_keyboard"
 cd "$src_path$ur_sub_dir"
 echo "Updating: $(pwd)"
 git checkout kinetic-devel
 git pull
-git submodule update --init --recursive
+cd "$src_path$teleop_twist_keyboard"
+echo "Updating: $(pwd)"
+git checkout master
+git pull
 cd "$src_path"
